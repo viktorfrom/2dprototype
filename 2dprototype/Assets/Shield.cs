47d3a0a8be82dbcde54eaa521afd77662a282c19
unknown <viktor.from91@gmail.com>
2021-05-13 17:13:01 +0000

    Add rotating shield

---
+++ 2dprototype/Assets/Shield.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Set Dynamically")]
+    public float levelShown = 0;
+
+    // This non-public variable will not appear on the Inspector
+    Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+
+    void Update()
+    {
+        // Read the current shield level from the Hero Singleton
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+
+        if (levelShown != currLevel)
+        {
+            levelShown = currLevel;
+            // Adjust the texture offset to show different shield level
+            mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);
+        }
+
+        // Rotate the shield a bit every frame in a time-based way
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+        
+    }
+}
