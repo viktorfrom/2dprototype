305a4915f1f6938c5f519e94d3d6409069f10754
unknown <viktor.from91@gmail.com>
2021-05-13 16:07:16 +0000

    Add hero script

---
+++ 2dprototype/Assets/Hero.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S; // Singleton
+
+    [Header("Set in inspector")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null) 
+        {
+            S = this; // Set the Singleton
+
+        }
+        else 
+        {
+            Debug.Log("LogError seems to prevent the program from ever starting");
+            // Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!")
+        }
+    }
+
+    void Update()
+    {
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        
+        // CHange transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+}
