e8602cecad9727893148cb4a9aea7cca9b98037b
unknown <viktor.from91@gmail.com>
2021-05-24 18:53:38 +0100

    Update Enemy_4 movement

--- 2dprototype/Assets/__Scripts/Enemy_4.cs
+++ 2dprototype/Assets/__Scripts/Enemy_4.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_4 : MonoBehaviour
+public class Enemy_4 : Enemy
 {
-    // Start is called before the first frame update
+    private Vector3 p0, p1;
+    private float timeStart;
+    private float duration = 4;
+
     void Start()
     {
-        
+        p0 = p1 = pos;
+
+        InitMovement();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitMovement()
     {
+        p0 = p1;
         
+        float widMinRad = bndCheck.camWidth - bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+        p1.x = Random.Range(-widMinRad, widMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        timeStart = Time.time;
+    }
+
+    public override void Move()
+    {
+        float u = (Time.time - timeStart) / duration;
+        if (u >= 1)
+        {
+            InitMovement();
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow(1 - u, 2);
+        pos = (1 - u) * p0 + u * p1;
     }
 }
